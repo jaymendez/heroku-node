0193408ee8d1e5a573cdf2206d3309153cd7b148
Jay Mendez <johnjosephmendez2016@gmail.com>
2019-07-31 18:40:50 +0800

    init commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/sms', function(req, res, next) {
+  // res.json({"test": "test"});
+  // console.log(req);
+  // console.log(res);
+  // res.render('index', { title: 'Express' });
+});
+
+router.post('/sms', function(req, res, next) {
+  // res.json({"test": "test"});
+  console.log(req.body);
+  // console.log(res);
+  // res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
