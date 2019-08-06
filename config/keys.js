75d24c41b33e8c7a30a332ec96a61f69d3cfb2ce
Jay Mendez <johnjosephmendez2016@gmail.com>
2019-08-06 14:54:06 +0800

    S

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,9 @@
+const USERNAME = "jaymndz";
+const PASSWORD = "jaymndz";
+const CLUSTER = "cluster0";
 module.exports = {
-  mongoURI: 'mongodb+srv://jaymndz:jaymndz@dev-connector-lcoec.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  // mongoURI: `mongodb+srv://${USERNAMAE}:${PASSWORD}@dev-connector-lcoec.gcp.mongodb.net/test?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${USERNAMAE}:${PASSWORD}@${CLUSTER}-npdc2.gcp.mongodb.net/test?retryWrites=true&w=majority`,
   secretOrKey: 'secret'
 }
 // mongodb+srv://jaymndz:<password>@dev-connector-lcoec.gcp.mongodb.net/test?retryWrites=true&w=majority
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+
+const Sms = require('../models/Sms');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,7 +22,17 @@ router.post('/sms', function(req, res, next) {
   // res.json({"test": "test"});
   console.log(req.body);
   if (req.body.inboundSMSMessageList) {
-    console.log(req.body.inboundSMSMessageList.inboundSMSMessage[0]);
+    // console.log(req.body.inboundSMSMessageList.inboundSMSMessage[0]);
+    let data = req.body.inboundSMSMessageList.inboundSMSMessage[0];
+    const receivedSms = new Sms({
+      date_received: data.dateTime,
+      destinationAddress: data.destinationAddress,
+      messageId: data.messageId,
+      message: data.message,
+      resourceURL: data.resourceURL,
+      senderAddress: data.senderAddress 
+    })
+    receivedSms.save().then(sms => res.json(sms));
   } 
 
   // console.log(res);
